65ee676250d7a65ee7654850f2ee47835d6c29e3
VladaSandul <123377041+VladaSandul@users.noreply.github.com>
2023-02-20 20:52:35 +0200

    Add files via upload

---
+++ Practikum_3/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Practikum_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+       //Задание 1
+
+            // int[] cost = { 12, 15, 5, 19, 8, 30, 4, 13, 20 };
+            // int maxVal = cost.Max();
+            // int indexMax = Array.IndexOf(cost, maxVal);
+
+            // Console.WriteLine($"Максимальный элемент массива: {maxVal}, индекс: {indexMax}");
+            // Console.ReadKey();
+
+
+          //Задание 2
+            //int[] a = { 3, 10, 4, 8, 12, 9, 5, 6, 2, 20 };
+            // int imax = 0;
+            // int imin = 0;
+            // int[] c = new int[20];
+            // Random r = new Random();
+            // for (int i = 0; i < a.Length; i++)
+            //     {
+            //         Console.Write((a[i] = r.Next(0, 99)) + " ");
+            //         if (a[imin] > a[i])
+            //             {
+            //                 imin = i;
+            //             }
+            //             if (a[imax] < a[i])
+            //             {
+            //                 imax = i;
+            //             }
+            //         }
+
+            //             int x = a[imin];
+            //             a[imin] = a[imax];
+            //             a[imax] = x;
+
+            //             Console.WriteLine();
+            //             for (int i = 0; i < a.Length; i++)
+            //                 Console.Write(a[i] + " ");
+            //             Console.ReadKey();
+
+
+            //Задание_4
+            // int[] suum = { 12, 15, 5, 19, 8, 30, 4, 13, 20 };
+            // int maxVal_1 = suum.Max();
+            // int minVal = suum.Min();
+
+            // int sum = suum.Max() + suum.Min();
+
+            // for(int i = 0; i < suum.Length; i++)
+            // {
+            //     Console.Write($"{suum[i] += sum}" + " ");
+            // }
+            // Console.ReadKey();
+        }
+    }
+}
