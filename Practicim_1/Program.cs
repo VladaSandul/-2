2882009b5d4b649152e3f97e07524031916e3d52
VladaSandul <123377041+VladaSandul@users.noreply.github.com>
2023-02-20 20:51:59 +0200

    Add files via upload

---
+++ Practicim_1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Practicim_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1
+            // int[] arr = new int[8];
+            // for(int i = 0; i < arr.Length; i++)
+            // {
+            //     Console.Write( $"[ {i + 1} ] = " );
+            //     arr[ i ] = Convert.ToInt32( Console.ReadLine() );
+            // }
+            // Console.ReadKey();
+
+
+            //Задание 3
+            // int[] bat = new int[] {-7, 10, 5, 16, -4, 18};
+            // Console.WriteLine("Элементы массива в обратном порядке: ");
+            // for(int i = bat.Length -1; i >=0; --i)
+            // {
+
+            //     Console.Write($"{bat[i]}  ");
+            // }
+            // Console.WriteLine();
+
+            //Задание 4
+
+            //int[] ros = new int[12];
+            //Random rand = new Random();
+            //Console.WriteLine("Элементы массива: ");
+            //for (int i = 0; i < ros.Length; i++)
+            //{
+            //    ros[i] = rand.Next(163, 191);
+            //    Console.Write(ros[i] + " ");
+            //}
+            //Console.WriteLine();
+            //Console.ReadKey();
+        }
+    }
+}
